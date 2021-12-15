233194af80fed939b10399e176c1bb69ee175cf7
wynnbwynn <wynn.bailey@gmail.com>
2021-12-15 09:35:07 -0800

    - add serving size picker

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe:Recipe
-    
+    @State var selectedServingSize = 2
     var body: some View {
         
         ScrollView {
@@ -21,7 +21,20 @@ struct RecipeDetailView: View {
                 Image(recipe.image)
                     .resizable()
                     .scaledToFill()
+                // MARK: Serving Size Picker
                 
+                VStack(alignment: .leading) {
+                    Text("Select your serving size")
+                    Picker("", selection: $selectedServingSize){
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 160)
+                }
+                .padding()
                 // MARK: Ingredients
                 VStack(alignment: .leading) {
                     Text("Ingredients")
